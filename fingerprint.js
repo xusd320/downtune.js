1bb47e5a58d97bf413fc541d0d091aa7dcd44166
xushaodong <xushaodong@guazi.com>
2019-05-15 01:17:44 +0800

    update fingerprint

--- fingerprint.js
+++ fingerprint.js
@@ -4,10 +4,6 @@ const stringify = require('json-stable-stringify');
 const crypto = require('crypto');
 
 module.exports = function fingerprint(request) {
-  const reqStr = stringify(request); 
-  const hash = crypto.createHash('sha256');
-  hash.update(reqStr);
-  const hex = hash.digest('hex');
-  return hex;
+  return crypto.createHash('sha256').update(stringify(request)).digest('hex');
 };
 
