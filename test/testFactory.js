239d168ad1bd5ac93e2b3e3f4493d9a5c66de193
xushaodong <xushaodong@guazi.com>
2019-07-17 14:12:35 +0800

    update testFactory

--- test/testFactory.js
+++ test/testFactory.js
@@ -1,17 +1,17 @@
 'use strict';
 const factory = require('../src/factory.js');
 
-const f = new factory(10);
+const f = new factory(100);
 
-let max = 1000000;
+let max = 1000;
 
-f.run((done) => {
+f.run(done => {
   max --;
-  if(max === 999900) f.stop();
-  else setTimeout(done,1000);
-  }).then(() => {
-    console.log('finish');
-    console.log('--------------------------------------');
-  });
+  if(max === 0) f.stop();
+   else setTimeout(done,1000);
+}).then(() => {
+  console.log('===== finish =====');
+  clearInterval(inter);
+});
 
-setInterval(() => console.log(max), 1000);
+const inter = setInterval(() => console.log(max), 1000);
